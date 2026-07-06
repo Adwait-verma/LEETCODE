738eb678e56a690aefe99d7e601f2c4733f95617
Adwait-verma <vermaadwait07@gmail.com>
2026-07-06 23:53:04 +0530

    Time: 3 ms (46.3%), Space: 10 MB (11.31%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        unordered_map<char,int> count;
+
+        if( size(s)!=size(t))
+        {
+            return false;
+        }
+        for ( char c: s)
+        {
+            count[c]++;
+        }
+        for ( char c :t)
+        {
+            count [c]--;
+
+        if ( count[c]<0)
+        {
+            return false;
+
+        }
+        
+        }
+        return true;
+        
+    }
+};
