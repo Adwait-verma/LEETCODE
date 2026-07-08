58bfcdde2e2d043f8661b8556f33d0a14f1ddb3f
Adwait-verma <vermaadwait07@gmail.com>
2026-07-08 11:41:35 +0530

    Time: 0 ms (100%), Space: 12.4 MB (9.52%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int k=m+n-1;
+        int l1=m-1;
+        int l2=n-1;
+        while ( l1 >=0 && l2 >=0)
+        {
+            if ( nums1[l1]>nums2[l2])
+            {
+                nums1[k]=nums1[l1];
+                k--;
+                l1--;
+            }
+            else
+            {
+                
+                nums1[k]=nums2[l2];
+                k--;
+                l2--;
+            }
+        }
+        while ( l2 >=0)
+        {
+            nums1[k]=nums2[l2];
+            k--;
+            l2--;
+        }
+
+    }
+};
